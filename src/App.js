61c2a7b847ac4aa235e51056bbcc296c1a03acf4
stewwxcode <stewartnadugaddi@gmail.com>
2022-05-10 18:11:46 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-const App = () => {
-  return (
-    <div>
-      Tech-Com
-    </div>
-  );
-}
+import { Route, Routes } from "react-router";
+import Loader from "./layout/Loader";
+import Home from './components/Home'
 
+const App = () => {
+  return ( 
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}  />
+      </Routes>
+    </div>
+   );
+}
+ 
 export default App;
