2bf16017ed02a0517b3f42e5105d1c496628f231
stewwxcode <stewartnadugaddi@gmail.com>
2022-05-12 16:35:45 +0530

    login page

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,19 +1,23 @@
-
 const initialState = {
-    token: null,
-    user: {
-        email:null,
-        id:null
-    },
-  };
-  
-  const authReducer = (state = initialState, action) => {
-    const { type, payload } = action;
-  
-    switch (type) {
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer
+  token: null,
+  user: {
+    email: null,
+    id: null,
+  },
+};
+
+const authReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "LOGIN_SUCCESS":
+      return { ...payload };
+    case "LOGIN_FAILED":
+      return state;
+    case "LOAD_USER":
+      return { ...payload };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
